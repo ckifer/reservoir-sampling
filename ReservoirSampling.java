c923e067c68059fd7861505878374c0dd6469e90
Coltin Kifer <ck108860@yahoo.com>
2019-12-04 11:54:43 -0500

    change outputs

--- ReservoirSampling.java
+++ ReservoirSampling.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class ReservoirSampling  
 { 
     // reservoir sampling is taking in a stream of numbers with a capacity K that 
-    static void selectSamples(int stream[], int n, int k) 
+    static int[] selectSamples(int stream[], int n, int k) 
     { 
         // define i 
         int i;  
@@ -14,16 +14,16 @@ public class ReservoirSampling
         // add the first k items of the stream into the reservoir
         for (i = 0; i < k; i++) 
             reservoir[i] = stream[i]; 
-          
+        
         // :O
         Random r = new Random(); 
         
         // switch out 'random' indexes if the value of j is an index in the reservoir
-        // i will start at 6 and 'maybe' swap items out until i is the length of the stream
+        // i will start at k (4) and 'maybe' swap items out until i is the length of the stream (12)
         for (; i < n; i++) 
         { 
-            // 'random' value between current 0 and i (k - 1)
-            int j = r.nextInt(i + 1); 
+            // 'random' value between current 0 and i (4)
+            int j = r.nextInt(i); 
               
             // If the randomly  picked index is smaller than k (the length of the reservoir), 
             // then replace the element present at the index 
@@ -31,9 +31,7 @@ public class ReservoirSampling
             if(j < k) 
                 reservoir[j] = stream[i];             
         } 
-          
-        System.out.println("These are 'randomly' selected items: "); 
-        System.out.println(Arrays.toString(reservoir)); 
+        return reservoir;
     } 
 
     // simply use random generator to generate a 'random' list of streamLength ints from 0 to 100 to be the stream input
@@ -51,8 +49,16 @@ public class ReservoirSampling
         int n = 12;
         // stream of 'random' ints
         int stream[] = generateStream(n); 
+        System.out.println("'Randomly' generated input: " + Arrays.toString(stream));
         // number of samples to choose from stream
-        int k = 6;
-        selectSamples(stream, n, k); 
+        int k = 4;
+
+        int[] reservoir = new int[k];
+        for(int i = 0; i < 5; i++){
+            reservoir = selectSamples(stream, n, k); 
+
+            System.out.println("These are 'randomly' selected items: "); 
+            System.out.println(Arrays.toString(reservoir));
+        }
     } 
 }
